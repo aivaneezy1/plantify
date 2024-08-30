8320d9a352bcebf30e427617de55c9a770a58123
aivaneezy1 <aivaneezydakii@gmail.com>
2024-08-30 19:59:41 +0200

    Added functionality to fetch images from S3 bucket using AWS SDK, Filtered out common prefixes and ensured only valid image URLs are returned and Updated API endpoint to support retrieving image URLs.

--- app/api/analyzeImage/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
-
-
-export const POST = async(request:NextRequest) =>{
-    
-}
--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -37,12 +37,12 @@ export default function ProductComponent() {
        
       >
         <MenuItem onClick={handleClose}>
-          <div className="flex flex-row justify-center items-center gap-2 hover:font-semibold">
+          <div className="flex flex-row justify-center items-center gap-2 transition transform hover:scale-105 duration-300 hover:font-semibold">
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
-                className="w-4 h-4"
+                className="w-4 h-4 "
               >
                 <path
                   fill="#B197FC"
@@ -54,7 +54,7 @@ export default function ProductComponent() {
           </div>
         </MenuItem>
         <MenuItem onClick={handleClose}>
-          <div className="flex flex-row justify-center items-center gap-2 hover:font-semibold ">
+          <div className="flex flex-row justify-center items-center gap-2 transition transform hover:scale-105 duration-300 hover:font-semibold ">
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -72,7 +72,7 @@ export default function ProductComponent() {
         </MenuItem>
 
            <MenuItem onClick={handleClose}>
-          <div className="flex flex-row justify-center items-center gap-2 hover:font-semibold">
+          <div className="flex flex-row justify-center items-center gap-2 transition transform hover:scale-105 duration-300 hover:font-semibold">
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4"><path fill="#63E6BE" d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>
             </div>
@@ -81,7 +81,7 @@ export default function ProductComponent() {
         </MenuItem>
 
          <MenuItem onClick={handleClose}>
-          <div className="flex flex-row justify-center items-center gap-2 hover:font-semibold">
+          <div className="flex flex-row justify-center items-center gap-2 transition transform hover:scale-105 duration-300 hover:font-semibold">
             <div>
              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="w-4 h-4"><path fill="#1b0ced" d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>
             </div>
