8da8a0c1317154bb9ba94c0bd90a2aeac88780d4
aivaneezy1 <aivaneezydakii@gmail.com>
2024-09-04 23:27:29 +0200

    added a authentication with clerk and convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://lenient-drake-95.clerk.accounts.dev/',
+      applicationID: 'convex',
+    },
+  ],
+}
---
+++ convex/createUser.ts
@@ -0,0 +1,47 @@
+import {
+  internalAction,
+  internalMutation,
+  mutation,
+} from "./_generated/server";
+import { v } from "convex/values";
+
+export const userTable = mutation({
+  args: {
+    firstName: v.string(),
+    lastName: v.string(),
+    email: v.string(),
+    apiCallTotal: v.number(),
+    apiCallRemaining: v.number(),
+    tokenIdentifier: v.string(),
+  },
+  handler: async (ctx, args) => {
+    // check if the user is currently logged in
+    const checkUser = await ctx.auth.getUserIdentity();
+    if (!checkUser) {
+      console.log("User is not logged in");
+    }
+    // check if user is already in our database
+    const usersExist = await ctx.db
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", args.tokenIdentifier)
+      )
+      .unique();
+
+    if (usersExist) {
+      return usersExist._id;
+    }
+    // If user dont exist in our database yet. we create one
+    const newUsers = await ctx.db.insert("users", {
+      firstName: args.firstName,
+      lastName: args.lastName,
+      email: args.email,
+
+      apiCallTotal: args.apiCallTotal,
+      apiCallRemaining: args.apiCallRemaining,
+      tokenIdentifier: args.tokenIdentifier,
+    });
+
+    return newUsers;
+  },
+});
