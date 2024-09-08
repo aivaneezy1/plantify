6f2bb18f074c8f8925fd44d0db7fbc6a5a3af397
aivaneezy1 <aivaneezydakii@gmail.com>
2024-09-09 00:57:19 +0200

    Added a frequest question component

---
+++ app/components/Hero-Images.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+const HeroImagesComponent = () => {
+  return (
+    <div>
+         <div className="mt-20 flex flex-col md:flex-row justify-center items-center gap-5 p-6">
+        <div className="text-center ">
+          <h2 className="text-4xl font-semibold mb-4 ">
+            Generate images from a text prompt
+          </h2>
+          <p className="text-lg mb-6">
+            Submit a text prompt and see it transformed into a stunning image.
+          </p>
+          <Link href="/generatePrompt">
+            <button className="bg-blue-500 text-white font-bold py-2 px-12 rounded hover:bg-blue-600 transition duration-300">
+              Generate
+            </button>
+          </Link>
+        </div>
+        <div className="flex justify-center items-center">
+          <Image
+            src="/ai.webp"
+            alt="First Image"
+            width={500}
+            height={500}
+            className="w-full h-auto object-cover rounded-lg shadow-lg transition transform hover:scale-105 duration-300 cursor-pointer"
+          />
+        </div>
+      </div>
+
+      <div className="mt-20 flex flex-col md:flex-row justify-center items-center gap-5 p-6">
+        <div className="flex justify-center items-center ">
+          <Image
+            src="/medium.webp"
+            alt="First Image"
+            width={500}
+            height={500}
+            className="w-full h-auto object-cover rounded-lg shadow-lg transition transform hover:scale-105 duration-300 cursor-pointer"
+          />
+        </div>
+        <div className=" text-center ">
+          <h2 className="text-4xl font-semibold mb-4">
+            Generate images from a sketch
+          </h2>
+          <p className="text-lg mb-6">
+           Upload a sketch and watch it come to life as a fully rendered image.
+          </p>
+          <Link href="/generateSketch">
+            <button className="bg-purple-500 text-white font-bold py-2 px-8 rounded hover:bg-purple-600 ">
+              Generate
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroImagesComponent
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,31 +5,30 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import Image from "next/image";
 import Loading from "../utils/Loading";
-
-interface ImageData   {
+import FrequentQuestionComponent from "./Frequent-Question";
+import HeroImagesComponent from "./Hero-Images";
+interface ImageData {
   key: string;
   url: string;
 }
 const Hero = () => {
-  const [dataImages, setDataImages] = useState<ImageData[]>([])
+  const [dataImages, setDataImages] = useState<ImageData[]>([]);
 
-
-  useEffect(() =>{
-    const handleGetData = async() =>{
-      try{
+  useEffect(() => {
+    const handleGetData = async () => {
+      try {
         const res = await fetch("api/s3-getImages");
-        if(res.ok){
+        if (res.ok) {
           const data = await res.json();
-       
+
           setDataImages(data);
-          
         }
-      }catch(err){
+      } catch (err) {
         throw new Error(`message: ${err}`);
       }
-    }
+    };
     handleGetData();
-  },[])
+  }, []);
   return (
     <div className="py-16 px-8 text-center flex flex-col justify-center items-center">
       <h2 className="text-5xl font-bold text-purple-500 mb-4">Plantify Ai</h2>
@@ -38,20 +37,19 @@ const Hero = () => {
       </p>
 
       <div className="mt-8 grid gap-4 md:grid-cols-4  grid-cols-3 ">
-      
         {dataImages.length > 0 ? (
           dataImages.map((src, idx) => (
-          <div key={idx}>
-            <Image
-                    src={src.url}
-                    alt={`Image ${idx}`}
-                    width={500}
-                    height={500}
-                    className="w-full h-auto object-cover transition transform hover:scale-105 duration-300 rounded-md"
-                  />
-          </div>
-      ))
-        ): (
+            <div key={idx}>
+              <Image
+                src={src.url}
+                alt={`Image ${idx}`}
+                width={500}
+                height={500}
+                className="w-full h-auto object-cover transition transform hover:scale-105 duration-300 rounded-md"
+              />
+            </div>
+          ))
+        ) : (
           <div className="flex justify-center flex-col mx-auto items-center text-white text-center">
             <Loading />
             <h2>Loading...</h2>
@@ -59,59 +57,12 @@ const Hero = () => {
         )}
       </div>
 
-      <div className="mt-20 flex flex-col md:flex-row justify-center items-center gap-5 p-6">
-        <div className="text-center ">
-          <h2 className="text-4xl font-semibold mb-4">
-            Generate images from a text prompt
-          </h2>
-          <p className="text-lg mb-6">
-            Provide a prompt and watch your prompt turned into an image
-          </p>
-          <Link href="/generatePrompt">
-            <button className="bg-blue-500 text-white font-bold py-2 px-12 rounded hover:bg-blue-600 transition duration-300">
-              Generate
-            </button>
-          </Link>
-        </div>
-        <div className="flex justify-center items-center">
-          <Link href="/generatePrompt">
-            <Image
-              src="/ai.webp"
-              alt="First Image"
-              width={500}
-              height={500}
-              className="w-full h-auto object-cover rounded-lg shadow-lg transition transform hover:scale-105 duration-300 cursor-pointer"
-            />
-          </Link>
-        </div>
-      </div>
 
-      <div className="mt-20 flex flex-col md:flex-row justify-center items-center gap-5 p-6">
-        <div className="flex justify-center items-center ">
-          <Link href="/generateSketch">
-            <Image
-              src="/medium.webp"
-              alt="First Image"
-              width={500}
-              height={500}
-              className="w-full h-auto object-cover rounded-lg shadow-lg transition transform hover:scale-105 duration-300 cursor-pointer"
-            />
-          </Link>
-        </div>
-        <div className=" text-center ">
-          <h2 className="text-4xl font-semibold mb-4">
-            Generate images from a sketch
-          </h2>
-          <p className="text-lg mb-6">
-            Provide a sketch, and watch the sketch turned into an image
-          </p>
-          <Link href="/generateSketch">
-            <button className="bg-purple-500 text-white font-bold py-2 px-8 rounded hover:bg-purple-600 ">
-              Generate
-            </button>
-          </Link>
-        </div>
-      </div>
+      {/*Hero images */}
+      <HeroImagesComponent/>
+
+      {/* Frequently Asked Questions */}
+      <FrequentQuestionComponent />
     </div>
   );
 };
